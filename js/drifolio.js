3654d56d900db07f93929ef29950c84d9eb0b0d0
srizon <m@creatrix.us>
2014-11-24 21:46:01 +0600

    Dribbble Follow button included

--- js/drifolio.js
+++ js/drifolio.js
@@ -20,6 +20,30 @@ $.jribbble.getShotsByPlayerId('srizon', function (playerShots) {
     $('#shotsByPlayerId').html(html.join(''));
 }, {page: 1, per_page: 9});
 
+//========================
+//Follow button
+//========================
+
+$(function() {
+
+
+	// SOME VARIABLES
+	var button = '.dribbble-follow-button',
+		label = $(button).text(),
+		username = $('a'+button).attr('href').toLowerCase().replace('http://dribbble.com/', ''),
+		disableCount = $(button).attr('class');
+
+	// DISPLAYED WHEN THE API IS NOT RESPONDING
+	$(button).wrap('<div class="dribbble-follow-button" />').removeClass().addClass('label').html('<i></i> '+label);
+
+	// REQUESTS USER'S DATA FROM DRIBBBLE'S API AND APPENDS IT
+	$.getJSON('http://api.dribbble.com/players/'+username+'?callback=?', function(data) {
+		$(button).wrap('<div class="dribbble-follow-button '+disableCount+'" />')
+        .parent().html('<a class="label" href="http://dribbble.com/'+username+'" target="_blank"><i></i>'+label+'</a><a class="count" href="http://dribbble.com/'+username+'/followers" target="_blank"><i></i><u></u>'+data.followers_count+' followers</a>');
+		$(button+'.disableCount').find('.count').remove();
+	});
+
+});
 
 
 //========================
