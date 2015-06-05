e93baa84087470c6a95a8fc7a7b8a07f02012984
Alex Mihov <mihov.alex@gmail.com>
2015-06-05 22:43:06 +0200

    [NEW] general: added browser-sync, git-ignore and gulp;

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+   gulp.watch("*.html").on("change", browserSync.reload);
+   gulp.watch("/css/*.css").on("change", browserSync.reload);
+});
